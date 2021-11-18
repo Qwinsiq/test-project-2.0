c794d2dcb87139f98d6a075ed346986c0564aafe
Konstantin <qwinsiq@gmail.com>
2021-11-18 11:33:14 +0200

    something added, now I don't understand what i need to do next

---
+++ test/include/NetConfAgent.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class NetConfAgent
+{
+    public:
+    bool subscribeForModelChanges();
+}
---
+++ test/source/NetConfAgent.cpp
@@ -0,0 +1,12 @@
+#include "NetConfAgent.hpp"
+#include <iostream>
+
+NetConfAgent::NetConfAgent()
+{
+
+}
+
+NetConfAgent::subscribeForModelChanges()
+{
+    std::cout<<"metod subscribeForModelChanges is called"<<std::endl;
+}
