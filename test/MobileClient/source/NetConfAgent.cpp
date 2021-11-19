711e8b48e177c342225b783d1255b85c8df30c56
Konstantin <qwinsiq@gmail.com>
2021-11-19 13:20:02 +0200

    some updates

--- test/MobileClient/source/NetConfAgent.cpp
+++ test/MobileClient/source/NetConfAgent.cpp
@@ -1,4 +1,4 @@
-#include "NetConfAgent.hpp"
+#include "/home/qwinsiq/Desktop/test_project/test/MobileClient/include/NetConfAgent.hpp"
 #include <iostream>
 
 NetConfAgent::NetConfAgent()
@@ -10,4 +10,11 @@ bool NetConfAgent::subscribeForModelChanges()
     connection=std::make_unique<sysrepo::Connection>();
     std::unique_ptr<sysrepo::Session> q (new sysrepo::Session(connection->sessionStart()));
     session=std::move(q);
+    session->copyConfig(sysrepo::Datastore::Startup, "test_module");
+    int called={0};
+    sysrepo::ModuleChangeCb moduleChangeCb = [&called] (auto, auto, auto, auto, auto, auto) -> sysrepo::ErrorCode {
+            called++;
+            return sysrepo::ErrorCode::Ok;
+        };
+    
 }
