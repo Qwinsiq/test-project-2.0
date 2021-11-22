f67029b1f8e3a4359078b41a99a49c15f05dab2b
Konstantin <qwinsiq@gmail.com>
2021-11-22 13:55:40 +0200

    some update

--- test/MobileClient/source/NetConfAgent.cpp
+++ test/MobileClient/source/NetConfAgent.cpp
@@ -9,12 +9,15 @@ void NetConfAgent::subscribeForModelChanges()
 {
     connection=std::make_unique<sysrepo::Connection>();
     auto  session= connection->sessionStart();
+    session.copyConfig(sysrepo::Datastore::Startup, "test_module");
     int called={0};
     sysrepo::ModuleChangeCb moduleChangeCb = [&called] (auto, auto, auto, auto, auto, auto) -> sysrepo::ErrorCode {
             called++;
             return sysrepo::ErrorCode::Ok;
         };
-    auto subscription=session.onModuleChange("testmodel", moduleChangeCb);
-    subscription.onModuleChange("testmodel",moduleChangeCb);
+    auto subscription=session.onModuleChange("test_module", moduleChangeCb);
+    subscription.onModuleChange("test_module",moduleChangeCb);
+    session.setItem("/test_module:leafInt32", "123");
+    session.applyChanges();
     std::cout<<"function called "<<called<<std::endl;
     }
